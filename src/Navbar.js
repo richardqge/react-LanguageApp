a7ea83ac1ab27983ed61268c18fa8dce5ccd0fcc
Roy Ge <rgecoder@gmail.com>
2020-02-26 13:15:18 -0800

    Add consume context inside PageContent.js and Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,19 +7,22 @@ import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
 import Switch from "@material-ui/core/Switch";
 import { withStyles } from "@material-ui/core/styles";
-import styles from './styles/NavBarStyles';
-
+import styles from "./styles/NavBarStyles";
+import { ThemeContext } from "./contexts/ThemeContext";
 class Navbar extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isDarkMode } = this.context;
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <AppBar position='static' color='primary'>
+        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
           <Toolbar>
-            <IconButton className={classes.menuButton} color='inherit'>
+            <IconButton className={classes.menuButton} color="inherit">
               <span>🇫🇷</span>
             </IconButton>
-            <Typography className={classes.title} variant='h6' color='inherit'>
+            <Typography className={classes.title} variant="h6" color="inherit">
               Language App
             </Typography>
             <Switch />
@@ -29,7 +32,7 @@ class Navbar extends Component {
                 <SearchIcon />
               </div>
               <InputBase
-                placeholder='Search...'
+                placeholder="Search..."
                 classes={{
                   root: classes.inputRoot,
                   input: classes.inputInput
