7d7af41e8e32b052b0ef92fcca2c2aacd6adc54c
Roy Ge <rgecoder@gmail.com>
2020-02-26 15:18:18 -0800

    Edit refactor LanguageContext.js to functional component, clean up code

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,26 +1,24 @@
-import React, { Component, createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { language: "spanish" };
-  }
-  changeLanguage=(e)=>{
-    this.setState({language: e.target.value});
-  }
-  render() {
-    return (
-      <LanguageContext.Provider value={{ ...this.state, changeLanguage: this.changeLanguage }}>
-        {this.props.children}
-      </LanguageContext.Provider>
-    );
-  }
+export function LanguageProvider(props) {
+  const [language, setLanguage] = useState("spanish");
+
+  const changeLanguage = e => setLanguage(e.target.value);
+  // changeLanguage=(e)=>{
+  //   this.setState({language: e.target.value});
+  // }
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
 }
 
-export const withLanguageContext = Component => props => (
-  <LanguageContext.Consumer>
-    {value =><Component languageContext={value} {...props}/>}
-  </LanguageContext.Consumer>
-)
+// export const withLanguageContext = Component => props => (
+//   <LanguageContext.Consumer>
+//     {value => <Component languageContext={value} {...props} />}
+//   </LanguageContext.Consumer>
+// );
